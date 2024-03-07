a1d1687a8254ce4ba65766da86be626cd49dcbaa
NANDO9322 <andre.fr93@gmail.com>
2024-03-07 10:49:57 -0300

    correção script

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
-function soma(a,b) {
+// src/index.js
+function soma(a, b) {
     return a + b;
-}
+  }
+  
+  function multiplicacao(a, b) {
+    return a * b;
+  }
+  
+  module.exports = { soma, multiplicacao };
+  
